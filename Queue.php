2515c0be6aec31b85128e78e45b77e05d36f9594
chnyang <chn.yang@qq.com>
2019-04-09 17:31:03 +0800

    双向队列

--- Queue.php
+++ Queue.php
@@ -59,13 +59,25 @@ class Queue implements Iterator
      * 非首节点,存储last的引用, last->prev指向oldLast
      * oldLast->next指向last
      */
-    public function enqueue($value)
+    public function addFirst($value)
     {
         if ($this->size == 0) {
+            $this->addFirstNode($value);
+        } else {
+            $oldFirst = $this->first;
             $this->first = new Node();
-            $this->last = $this->first;
             $this->first->value = $value;
-            $this->position = $this->first;
+            $this->first->next = $oldFirst;
+            $oldFirst->prev = $this->first;
+        }
+        $this->size++;
+        return true;
+    }
+
+    public function addLast($value)
+    {
+        if ($this->size == 0) {
+            $this->addFirstNode($value);
         } else {
             $oldLast = $this->last;
             $this->last = new Node();
@@ -81,21 +93,40 @@ class Queue implements Iterator
      * 出队操作, 队列为空返回异常, first指向first->next
      * @throws Exception
      */
-    public function dequeue(): int
+    public function lastPop(): int
     {
         if ($this->isEmpty()) {
-            throw new Exception("Queue is null");
+            throw new Exception("Queue is empty");
         }
         $value = $this->first->value;
         $this->first = $this->first->next;
         $this->size--;
         return $value;
     }
+
+    public function firstPop(): int
+    {
+        if ($this->isEmpty()) {
+            throw new Exception("Queue is empty");
+        }
+        $value = $this->last->value;
+        $this->last = $this->last->prev;
+        $this->size--;
+        return $value;
+    }
+
+    private function addFirstNode($value)
+    {
+        $this->first = new Node();
+        $this->last = $this->first;
+        $this->first->value = $value;
+        $this->position = $this->first;
+    }
 }
 
 $queue = new Queue();
 for ($i = 1; $i <= 10; $i++) {
-    $queue->enqueue($i);
+    $queue->addFirst($i);
 }
 
 echo "length = " . $queue->length() . PHP_EOL;
@@ -103,11 +134,16 @@ echo "length = " . $queue->length() . PHP_EOL;
 
 for ($i = 1; $i <= 5; $i++) {
     if ($queue->length() !== 0) {
-        echo $queue->dequeue() . "出队" . PHP_EOL;
+        echo $queue->lastPop() . "出队" . PHP_EOL;
     }
 }
 
-
 foreach ($queue as $value) {
     var_dump($value);
+}
+
+for ($i = 1; $i <= 5; $i++) {
+    if ($queue->length() !== 0) {
+        echo $queue->firstPop() . "出队" . PHP_EOL;
+    }
 }
